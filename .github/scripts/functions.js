5da00bcb96876cfeeabfa5d22b417cdfbd7cf283
Bruno Tetzner <brunoltetzner@gmail.com>
2021-10-21 12:01:45 -0300

    Finalizando funçoes

--- .github/scripts/functions.js
+++ .github/scripts/functions.js
@@ -94,49 +94,152 @@ function reduceA(acumulador, valorAtual) { return acumulador + valorAtual; }
 function newReduce(callback, array) {
     let soma = 0
     for (let indice = 0; indice < array.length; indice++) {
-        soma = callback(soma,array[indice])
+        soma = callback(soma, array[indice])
     }
-    return soma 
+    return soma
 }
-console.log(newReduce(reduceA,arrayofNumbers))
+//console.log(newReduce(reduceA,arrayofNumbers))
 
 
-/*-----------------SOME------------*/ 
-function writeSome(item,indice, array){
-    if(indice > 2 && item > 4){
+/*-----------------SOME------------*/
+function writeSome(item, indice, array) {
+    if (indice > 2 && item > 4) {
         return true
     }
-    {return false}
+    { return false }
 }
-function newSome(callback,array){
+function newSome(callback, array) {
     let boolean = false
-    for(let indice = 0; indice<array.length; indice++){
-        if(callback(array[indice],indice,array) === true){
+    for (let indice = 0; indice < array.length; indice++) {
+        if (callback(array[indice], indice, array) === true) {
             boolean = true
         }
     }
     return boolean
 }
 
-console.log(newSome(writeSome,arrayofNumbers))
+//console.log(newSome(writeSome,arrayofNumbers))
 
 /*--------------Every-----------------*/
-function writeEvery(item, indice, array){
-    if(indice > 2 && item > 4){
+function writeEvery(item, indice, array) {
+    if (item > 0 && indice >= 0) {
         return true
     }
-    {return false}
+    { return false }
 }
-function newEvery(callback,array){
+function newEvery(callback, array) {
     let boolean
-    for(let indice = 0; indice < array.length; indice ++){
-        if(writeEvery(array[indice],indice, array) === true){
+    for (let indice = 0; indice < array.length; indice++) {
+        if (callback(array[indice], indice, array) === true) {
             boolean = true
         }
-        else{
+        else {
             boolean = false
             break
         }
-        return boolean
     }
+    return boolean
+
 }
+//console.log(newEvery(writeEvery,arrayofNumbers))
+/*----------------------- FILL -------------*/
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+function NewFill(array, newElement, play, end) {
+    if (play === undefined) {
+        play = 0;
+    }
+    if (end === undefined) {
+        end = array.length - 1
+    }
+    else if (end > array.length - 1) {
+        end = array.length - 1
+    }
+    for (let indice = play; indice <= end; indice++) {
+        array[indice] = newElement
+    }
+    return fruits
+}
+
+//console.log(NewFill(fruits,'Kiwi',2,5))
+
+/*----------------includes--------------*/
+function newIncludes(item, array) {
+    let boolean = false
+    for (let indice = 0; indice < array.length; indice++) {
+        if (array[indice] === item) {
+            boolean = true
+        }
+    }
+    return boolean
+}
+//console.log(newIncludes(1,arrayofNumbers))
+
+/*--------------indexOf*/
+function newIndexOf(array, item, play, end) {
+    if (play === undefined) {
+        play = 0
+    }
+    if (end === undefined) {
+        end = array.length - 1
+    }
+    else if (end > array.length - 1) {
+        end = array.length - 1;
+    }
+
+    for (let indice = play; indice <= end; indice++) {
+        if (array[indice] === item) {
+            return indice
+        }
+    }
+    return -1
+}
+//console.log(newIndexOf(arrayofNumbers,1))
+
+/*-------------------concat-----------------*/
+let fruit = ['maça', 'banana', 'pera', 'uva']
+let WeekDays = ['segunda', 'terça', 'quarta', 'quinta', 'sexta']
+
+function newConcat(array, arrayConcat) {
+    for (let indice = 0; indice < arrayConcat.length; indice++) {
+        array.push(arrayConcat[indice])
+    }
+    return array
+}
+
+//console.log(newConcat(fruit, WeekDays))
+
+/*-----------------------Join--------------------------------------*/
+function newJoin(array, caracter) {
+    if (caracter === undefined) {
+        caracter = ','
+    }
+    let string = array[0]
+    for (let indice = 1; indice < array.length; indice++) {
+        string += `${caracter}${array[indice]}`
+    }
+    return string
+}
+
+//console.log(newJoin(arrayofNumbers,'------'))
+
+/*-----------------------slice------------------------------------*/
+let arrayToSlice = [1,2,3,4,5,6,7,8]
+function newSlice(array, start,end){
+    if(start === undefined){
+        start = 0;
+    }
+    if(end  === undefined){
+        end = array.length
+    }
+    else if(end > array.length){
+        end = array.length 
+    }
+    let finalArray = []
+    for (let indice = start; indice <end;indice++){
+        finalArray.push(array[indice])
+    }
+    return finalArray
+}
+//console.log(newSlice(arrayToSlice,0,70))
